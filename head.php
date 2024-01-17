278f2713b68be5e9035dc687d2f16de321517efb
Echamma <151067103+Echamma@users.noreply.github.com>
2024-01-16 20:03:44 -0600

    changing about me into index.php

--- head.php
+++ head.php
@@ -6,7 +6,7 @@
     <h1><?php echo $title?></h1>
     <nav class="navbar">
         <ul>
-            <li><a href="AboutMe.php">About Me</a></li>
+            <li><a href="index.php">About Me</a></li>
             <li><a href="Projects.php">Projects</a></li>
             <li><a href="Assignments.php">Assignments</a></li>
         </ul>
