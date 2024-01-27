611507aa8702faaf9565f9b6888fe8443a325953
Echamma <151067103+Echamma@users.noreply.github.com>
2024-01-27 15:32:50 -0600

    smol changes

--- src/views/header.php
+++ src/views/header.php
@@ -7,8 +7,8 @@ include_once("head.view.php");
     <h1><?php echo $title?></h1>
     <nav>
         <ul>
-            <li><a href=<?php echo APP_ROOT . "/articles/index.php"?>>About Me</a></li>
-            <li><a href=<?php echo APP_ROOT . "/articles/Assignments.php"?>>Assignments</a></li>
+            <li><a href=<?php echo $URL_BASE . "/articles/Aboutme.php"?>>About Me</a></li>
+            <li><a href=<?php echo $URL_BASE . "/articles/Assignments.php"?>>Assignments</a></li>
         </ul>
     </nav>
 </header>
