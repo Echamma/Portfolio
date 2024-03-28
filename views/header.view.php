25234daa014a75eb1c35ae7c8d1e82c49164e88d
Elias Chamma <elias.john.chamma@gmail.com>
2024-03-28 07:53:47 -0500

    Started file system and added views

---
+++ views/header.view.php
@@ -0,0 +1,3 @@
+<header>
+    <h1><?php $title ?></h1>
+</header>
---
+++ views/navbar.view.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">ContactMe</a></li>
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Experience</a></li>
+    </ul>
+</nav>
