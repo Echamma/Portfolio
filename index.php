f8e4ac3a4931a95dcc58585a7fc152c6123c2e7c
Elias Chamma <elias.john.chamma@gmail.com>
2024-04-01 12:36:36 -0500

    Fixed Configuration

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
 $title = "home";
+$config = parse_ini_file('config.ini', true);
+$environment= $config['ENVIRONMENT'];
+$url = $config[$environment]['URL_ROOT'];
+$app = $config[$environment]['URL_ROOT'];
+
